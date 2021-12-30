4b87a273ec8cafbb166d2a3b548da6becc5b2008
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-30 10:57:32 +0700

    Lab 3

--- B3/Lab3/B3/main.js
+++ B3/Lab3/B3/main.js
@@ -1,26 +1,35 @@
 import prompt from "prompts";
 import { FractionFormat } from "./Fraction.js";
 
+// Format
 const format = new FractionFormat("$ / %");
 
+// Example 
 console.log('Example fraction: 5 / 6');
 
+// Translate the input value
 const frac1 = format.translate(
+    // Input value
     (await prompt({
         type: 'text',
         name: 'value',
         message: 'Enter first fraction: '
     })).value
-), frac2 = format.translate(
-    (await prompt({
-        type: 'text',
-        name: 'value',
-        message: 'Enter second fraction: '
-    })).value
-);
+),
+    // Translate the input value
+    frac2 = format.translate(
+        // Input value
+        (await prompt({
+            type: 'text',
+            name: 'value',
+            message: 'Enter second fraction: '
+        })).value
+    );
 
+// Always simplify after doing some calculation
 frac1.alwaysSimplify = true;
 
+// Calculations
 console.log(format.apply(frac1.plus(frac2)));
 console.log(format.apply(frac1.minus(frac2)));
 console.log(format.apply(frac1.multiply(frac2)));
